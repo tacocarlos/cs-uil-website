55db65aabd7dbd77ec0de618489f93f76001522f
Carlos Luna <carlosluna3801@gmail.com>
2025-09-08 13:20:33 -0500

    allow sign-in

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -3,9 +3,9 @@ import SignInPage from "./signin";
 import { env } from "~/env";
 
 export default function SignIn() {
-    if (env.NODE_ENV === "development") {
-        return <SignInPage isLoading={false} />;
-    }
+    // if (env.NODE_ENV === "development") {
+    //     return <SignInPage isLoading={false} />;
+    //  }
 
     return <div>Signing in is not available at this time.</div>;
 }
