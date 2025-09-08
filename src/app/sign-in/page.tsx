6203092e59ff2877773c0c9eecff44acfc223637
Carlos Luna <carlosluna3801@gmail.com>
2025-09-08 13:24:25 -0500

    fixed logic

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -3,9 +3,5 @@ import SignInPage from "./signin";
 import { env } from "~/env";
 
 export default function SignIn() {
-    // if (env.NODE_ENV === "development") {
-    //     return <SignInPage isLoading={false} />;
-    //  }
-
-    return <div>Signing in is not available at this time.</div>;
+    return <SignInPage isLoading={false} />;
 }
